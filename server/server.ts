97b543fd566a5b0f5d361ab859ffb8bfa5668491
Eugenio Valeiras <eugenio.valeiras@gmail.com>
2017-09-05 19:18:42 -0300

    add cors

--- server/server.ts
+++ server/server.ts
@@ -7,20 +7,15 @@ var PORT = process.env.PORT || 3333;
 
 import * as express from 'express';
 import * as os from 'os';
-import * as http2 from 'spdy';
 import * as http from 'http';
-import * as fs from 'fs';
 import * as cors from 'cors';
 
 import {RoutesConfig} from './config/routes.conf';
-import {DBConfig} from './config/db.conf';
-import {JWTConf} from './config/jwt.conf';
-import {ViewerJSConf} from './config/viewerJS.conf';
 
 import {Routes} from './routes/index';
 
 const app = express();
-app.options('*', cors()); // include before other routes
+app.options(cors());
 
 console.log(`enviroment: ${process.env.NODE_ENV}`);
 
@@ -29,35 +24,14 @@ app.get('*', function (req, res, next) {
     res.redirect((process.env.HOSTURL || 'https://localhost:4200') + req.url)
   } else {
     next();
-    /* Continue to other routes if we're not redirecting */
   }
 })
 
 RoutesConfig.init(app);
-// DBConfig.init();
 Routes.init(app, express.Router());
-// JWTConf.init(app);
-// ViewerJSConf.init(app);
-
-//if (process.env.HEROKU){
 
 const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`up and running http @: ${os.hostname()} on port: ${PORT}`);
 });
-
-//} else {
-//
-//  const opts = {
-//    key: fs.readFileSync(__dirname + '/cert/server.key'),
-//    cert: fs.readFileSync(__dirname + '/cert/server.crt')
-//  };
-//
-//  http2.createServer(opts, app)
-//    .listen(PORT, () => {
-//      console.log(`up and running https @: ${os.hostname()} on port: ${PORT}`);
-//      console.log(`enviroment: ${process.env.NODE_ENV}`);
-//    });
-//
-//}
 
